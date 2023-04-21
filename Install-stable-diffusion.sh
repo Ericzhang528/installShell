2a4da6c05e0c9de0a91196ff58851c6bd19f3fb9
Ericzhang528 <54666403+Ericzhang528@users.noreply.github.com>
2023-04-21 16:27:32 +0800

    Update Install-stable-diffusion.sh

--- Install-stable-diffusion.sh
+++ Install-stable-diffusion.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
 # Step 0
-echo "拉取 stable-diffusion-webui 项目"
+echo -e "\033[34m拉取 stable-diffusion-webui 项目\033[0m"
 
 # Step 1
 git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git
 
 # Step 2
-echo "切换到 stable-diffusion-webui 目录"
+echo -e "\033[34m切换到 stable-diffusion-webui 目录\033[0m"
 cd stable-diffusion-webui
 
 # Step 3
-echo "新建 repositories 文件夹"
+echo -e "\033[34m新建 repositories 文件夹\033[0m"
 mkdir repositories
 
 # Step 4
-echo "拉取 StableDiffusion 依赖模型"
+echo -e "\033[34m拉取 StableDiffusion 依赖模型\033[0m"
 git clone https://github.com/CompVis/stable-diffusion.git repositories/stable-diffusion
 
 # Step 5
-echo "拉取 taming-transformers 依赖模型"
+echo -e "\033[34m拉取 taming-transformers 依赖模型\033[0m"
 git clone https://github.com/CompVis/taming-transformers.git repositories/taming-transformers
 
 # Step 6
-echo "拉取 CodeFormer 依赖模型"
+echo -e "\033[34m拉取 CodeFormer 依赖模型\033[0m"
 git clone https://github.com/sczhou/CodeFormer.git repositories/CodeFormer
 
 # Step 7
-echo "请输入您要使用的端口号："
+echo -e "\033[34m请输入您要使用的端口号：\033[0m"
 read PORT
 COMMANDLINE_ARGS="--medvram --always-batch-cond-uncond --port $PORT" REQS_FILE="requirements.txt" python launch.py
