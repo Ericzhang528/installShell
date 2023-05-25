c7ff2c870ac043e5a2795859dc9befe8b23af7c4
Ericzhang528 <54666403+Ericzhang528@users.noreply.github.com>
2023-05-25 17:47:02 +0800

    Update Centos7-Install-python3.10-pip.sh

--- Centos7-Install-python3.10-pip.sh
+++ Centos7-Install-python3.10-pip.sh
@@ -29,13 +29,14 @@ echo -e "\033[34mBuild and install Python\033[0m"
 sudo make -j$(nproc) altinstall
 
 # Install pip
-echo -e "\033[34mInstall pip\033[0m"
+echo -e "\033[34mInstall the latest version of pip\033[0m"
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 sudo python3.10 get-pip.py
 
-# Create a symlink from python to python3
-echo -e "\033[34mCreate a symlink from python to python3\033[0m"
-sudo ln -s /usr/local/bin/python3.10 /usr/local/bin/python
+# Create symlinks for python and pip commands
+echo -e "\033[34mCreate symlinks for python and pip commands\033[0m"
+sudo ln -sf /usr/local/bin/python3.10 /usr/local/bin/python
+sudo ln -sf /usr/local/bin/pip3.10 /usr/local/bin/pip
 
 # Verify installation
 echo -e "\033[34mVerify installation\033[0m"
